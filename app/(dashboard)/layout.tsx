58d6a529c688105dd27f2a6f2adb611fe1fc5664
Anushka <anushkakohli2021@gmail.com>
2024-04-17 18:07:28 +0530

    deployment corrections

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,10 +8,11 @@ const DashboardLayout = async ({
     children: React.ReactNode;
 }) => {
     const apiLimitCount = await getApiLimitCount();
+    const isPro = false;
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-800">
-                <Sidebar apiLimitCount={apiLimitCount} />
+                <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
             </div>
             <main className="md:pl-72">
                 <Navbar />
--- components/mobileSidebar.tsx
+++ components/mobileSidebar.tsx
@@ -33,7 +33,7 @@ const MobileSidebar = ({ apiLimitCount = 0, isPro = false }: MobileSidebarProps)
                 </Button>
             </SheetTrigger>
             <SheetContent side="left" className="p-0">
-                <Sidebar apiLimitCount={apiLimitCount} />
+                <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
             </SheetContent>
         </Sheet>
     );
